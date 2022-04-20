d3f617579aa971e465de0fe0fbd36e65420709fe
mobin <996833461@qq.com>
2022-04-20 18:40:02 +0800

    init:config pkg

---
+++ flink-connectors/flink-connector-redis/src/main/java/com/mobin/flink/connectors/redis/common/config/handler/FlinkJedisClusterConfigHandler.java
@@ -0,0 +1,45 @@
+package com.mobin.flink.connectors.redis.common.config.handler;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.flink.util.Preconditions;
+
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+/**
+ * jedis cluster config handler to find and create jedis cluster config use meta.
+ */
+public class FlinkJedisClusterConfigHandler implements FlinkJedisConfigHandler {
+
+    @Override
+    public FlinkJedisConfigBase createFlinkJedisConfig(Map<String, String> properties) {
+        Preconditions.checkArgument(properties.containsKey(RedisValidator.REDIS_NODES), "nodes should not be null in cluster mode");
+        String nodesInfo = properties.get(RedisValidator.REDIS_NODES);
+        Set<InetSocketAddress> nodes = Arrays.stream(nodesInfo.split(",")).map(r -> {
+            String[] arr = r.split(":");
+            return new InetSocketAddress(arr[0].trim(), Integer.parseInt(arr[1].trim()));
+        }).collect(Collectors.toSet());
+        String clusterPassword = properties.getOrDefault(RedisValidator.REDIS_CLUSTER_PASSWORD, null);
+        FlinkJedisClusterConfig.Builder builder = new FlinkJedisClusterConfig.Builder();
+        builder.setNodes(nodes);
+        if (StringUtils.isNotBlank(clusterPassword)) {
+            builder.setPassword(clusterPassword);
+        }
+        return builder.build();
+    }
+
+    @Override
+    public Map<String, String> requiredContext() {
+        Map<String, String> require = new HashMap<>();
+        require.put(RedisValidator.REDIS_MODE, RedisValidator.REDIS_CLUSTER);
+        return require;
+    }
+
+    public FlinkJedisClusterConfigHandler() {
+    }
+}
---
+++ flink-connectors/flink-connector-redis/src/main/java/com/mobin/flink/connectors/redis/common/config/handler/FlinkJedisSentinelConfigHandler.java
@@ -0,0 +1,36 @@
+package com.mobin.flink.connectors.redis.common.config.handler;
+
+
+
+import java.util.*;
+
+public class FlinkJedisSentinelConfigHandler implements FlinkJedisConfigHandler {
+
+    @Override
+    public FlinkJedisConfigBase createFlinkJedisConfig(Map<String, String> properties) {
+        String masterName = properties.computeIfAbsent(RedisValidator.REDIS_MASTER_NAME, null);
+        String sentinelsInfo = properties.computeIfAbsent(RedisValidator.SENTINELS_INFO, null);
+        Objects.requireNonNull(masterName, "master should not be null in sentinel mode");
+        Objects.requireNonNull(sentinelsInfo, "sentinels should not be null in sentinel mode");
+        Set<String> sentinels = new HashSet<>(Arrays.asList(sentinelsInfo.split(",")));
+        String sentinelsPassword = properties.computeIfAbsent(RedisValidator.SENTINELS_PASSWORD, null);
+        if (sentinelsPassword != null && sentinelsPassword.trim().isEmpty()) {
+            sentinelsPassword = null;
+        }
+        FlinkJedisSentinelConfig flinkJedisSentinelConfig = new FlinkJedisSentinelConfig.Builder()
+                .setMasterName(masterName).setSentinels(sentinels).setPassword(sentinelsPassword)
+                .build();
+        return flinkJedisSentinelConfig;
+    }
+
+    @Override
+    public Map<String, String> requiredContext() {
+        Map<String, String> require = new HashMap<>();
+        require.put(RedisValidator.REDIS_MODE, RedisValidator.REDIS_SENTINEL);
+        return require;
+    }
+
+    public FlinkJedisSentinelConfigHandler() {
+
+    }
+}
